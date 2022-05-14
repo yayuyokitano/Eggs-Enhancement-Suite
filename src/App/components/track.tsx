0a5a47d944eb4d1db17b86189bfbbf3d06cf3015
yayuyokitano <69117606+yayuyokitano@users.noreply.github.com>
2022-05-14 23:33:52 +0800

    Add data to track component

--- src/App/components/track.tsx
+++ src/App/components/track.tsx
@@ -1,5 +1,8 @@
 import { defaultAvatar } from "../../util/util";
 import { SongData } from "../../util/wrapper/eggs/artist";
+import PlayArrowRoundedIcon from '@mui/icons-material/PlayArrowRounded';
+import FavoriteRoundedIcon from '@mui/icons-material/FavoriteRounded';
+import ModeCommentRoundedIcon from '@mui/icons-material/ModeCommentRounded';
 import "./track.scss";
 
 function setPlayback(track:SongData) {
@@ -24,6 +27,22 @@ export function Track(props:{track:SongData, size:"normal"}) {
       <div className="ees-track-info">
         <span className="ees-track-title">{track.musicTitle}</span>
         <span className="ees-artist-name">{track.artistData.displayName}</span>
+        <div className="ees-track-details">
+          <div>
+            <PlayArrowRoundedIcon className="ees-data-icon" />
+            <span className="ees-data-count">{track.numberOfMusicPlays}</span>
+          </div>
+          <div>
+            <FavoriteRoundedIcon className="ees-data-icon" />
+            <span className="ees-data-count">{track.numberOfLikes}</span>
+          </div>
+          <div>
+            <ModeCommentRoundedIcon className="ees-data-icon" />
+            <span className="ees-data-count">{track.numberOfComments}</span>
+          </div>
+          
+
+        </div>
       </div>
     </li>
   )
