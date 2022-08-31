91c1640c7041ebd5520871a221746d99e1f09118
yayuyokitano <69117606+yayuyokitano@users.noreply.github.com>
2022-08-31 20:18:38 +0800

    Add profile banner to artist pages

--- src/App/artist/artist.tsx
+++ src/App/artist/artist.tsx
@@ -1,44 +1,44 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import { TFunction } from "react-i18next";
 import "./artist.scss";
 import { artistTracks, SongData } from "../../util/wrapper/eggs/artist";
 import TrackContainer from "../components/track/trackContainer";
 import { List } from "../../util/wrapper/eggs/util";
+import { crawlUser } from "../../util/wrapper/eggshellver/user";
+import { UserStub } from "../../util/wrapper/eggshellver/util";
+import ProfileBanner from "../components/profileBanner";
 
 export default function Artist(t:TFunction) {
-	const artistElement = document.getElementsByClassName("header_mypage_wrapper")[0];
-  
 	const artistID = window.location.pathname.split("/")[2];
 
 	const [data, setData] = useState<List<SongData>>();
 	const [isLoading, setLoading] = useState(false);
-	const ref = useRef<HTMLDivElement>(null);
+	const [user, setUser] = useState<UserStub>();
+	const userPromise = Promise.resolve(crawlUser());
 
 	useEffect(() => {
-		if (ref.current !== null) {
-			ref.current.appendChild(artistElement);
-		}
 		setLoading(true);
+		userPromise.then(u => {
+			setUser(u);
+		});
 		artistTracks(artistID).then((artistData) => {
 			setData(artistData);
 			setLoading(false);
-			if (ref.current === null) {
-				return <p>{t("general.error")}</p>;
-			}
-			ref.current.appendChild(artistElement);
 		});
 	}, []);
   
+	if (!user) return <div id="ees-artist">{t("general.loading")}</div>;
 	if (isLoading) return (
 		<div id="ees-artist">
-			<div ref={ref} />
+			<ProfileBanner user={user} />
+			<h2>{t("general.song", {count: data?.totalCount})}</h2>
 			{t("general.loading")}
 		</div>
 	);
 
 	return (
 		<div id="ees-artist">
-			<div ref={ref} />
+			<ProfileBanner user={user} />
 			<h2>{t("general.song", {count: data?.totalCount})}</h2>
 			<TrackContainer
 				data={data?.data}
